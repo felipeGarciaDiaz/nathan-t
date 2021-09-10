921413755ee8979de9c6666f66adf1da45c593ff
Nathan Tang <nathantang211@gmail.com>
2021-09-09 22:43:55 -0400

    major front end changes. Added interactive illustration section/gallery and dynamic background.

---
+++ app.js
@@ -0,0 +1,40 @@
+const slideShow = document.querySelector('.slide1');
+slideShow.style.transition = "transform 0.3s ease-in";
+const imgs = document.querySelectorAll('.slide1 img');
+
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const style = getComputedStyle(slideShow);
+var translatedX = 0;
+
+//arrow function
+next.addEventListener('mousedown', () => {
+    if (translatedX >= -1700) {
+        translatedX -= 200;
+        slideShow.style.transform += 'translateX(-200px)';
+    }
+});
+
+prev.addEventListener('mousedown', () => {
+    if (translatedX <= -100) {
+        translatedX += 200;
+        slideShow.style.transform += 'translateX(200px)';
+    }
+});
+
+function scrollToIntro() {
+    document.querySelector('#introid').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToProj() {
+    document.querySelector('#projectid').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToIllus() {
+    document.querySelector('#illusid').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToContact() {
+    document.querySelector('#getintouch').scrollIntoView({ behavior: 'smooth' });
+}
+
