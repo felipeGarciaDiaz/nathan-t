ee9fbfe69d0cafdde08d5eec2d1c29eccf8e0a9e
Nathan Tang <nathantang211@gmail.com>
2021-09-12 19:47:05 -0400

    finishing touches and info added to standing desk project!

--- projapp.js
+++ projapp.js
@@ -1,4 +1,4 @@
-var height;
+var height, topBar;
 const button = document.querySelector(".openmenu");
 const menu = document.querySelector(".menu");
 const menuicon = document.querySelector(".menuicon");
@@ -7,6 +7,35 @@ button.addEventListener('click', function () {
     menu.classList.toggle("visible");
     menuicon.classList.toggle("flip");
 });
+
+function checkBarSize(elementString) {
+    topBar = document.getElementById(elementString).offsetTop - $('.sidebar').height() - 10;
+    window.scroll({ top: topBar, behavior: "smooth" });
+}
+
+function scrollToA() {
+    checkBarSize("#a");
+}
+
+function scrollToB() {
+    checkBarSize("#b");
+}
+
+function scrollToC() {
+    checkBarSize("#c");
+}
+
+function scrollToD() {
+    checkBarSize("#d");
+}
+
+function scrollToE() {
+    checkBarSize("#e");
+}
+
+function scrollToF() {
+    checkBarSize("#f");
+}
 
 
 height = $('.mainsplash').height();
